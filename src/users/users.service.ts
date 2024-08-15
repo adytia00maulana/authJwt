e17d9aff05b00828a8e3c968cd6f06c32dee50f5
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-15 09:33:33 +0700

    add in User Service: change get id only author not null

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -16,7 +16,12 @@ export class UsersService {
   }
 
   findOne(id: number) {
-    return this.prisma.user.findUnique({ where: { id } });
+    return this.prisma.article.findUnique({
+      where: { id },
+      include: {
+        author: true,
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
