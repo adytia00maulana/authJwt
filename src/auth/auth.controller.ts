bf32931d90ac2006b563d28d5f9caf1b93d632eb
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-15 11:12:20 +0700

    add Auth in Auth Controller: add Tag Api for documentation SWAGGER UI

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from './auth.service';
 import { LoginDto } from "./dto/login.dto";
-import { ApiOkResponse } from "@nestjs/swagger";
+import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
 import { AuthEntity } from "./entities/auth.entity";
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
