086cf72f4757d51d59480f0561cabed58900a050
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-15 08:57:02 +0700

    Add: Users Entity with Api Property for Documentation SWAGGER UI

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class User {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  password: string;
+}
