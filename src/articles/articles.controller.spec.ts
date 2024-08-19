f89103b6bed56a611b7cb5c7894b647e28848cf5
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-19 13:43:23 +0700

    add Unit Testing for Articles Controller: Remove Create Update

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -3,29 +3,8 @@ import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { Logger } from '@nestjs/common';
-import { UpdateArticleDto } from "./dto/update-article.dto";
-import { CreateArticleDto } from "./dto/create-article.dto";
-
 describe('ArticlesController', () => {
   let controller: ArticlesController;
-
-  const mockData = {
-    id: 1,
-    title: "Prisma Adds Support for MongoDB",
-    description: "We are excited to share that today's Prisma ORM release adds stable support for MongoDB!",
-    body: "Support for MongoDB has been one of the most requested features since the initial release of...",
-    published: false,
-    createdAt: "2024-08-15T01:40:19.966Z",
-    updatedAt: "2024-08-15T07:37:13.594Z",
-    authorId: 1,
-    author: {
-      id: 1,
-      name: "Admin",
-      email: "admin@gmail.com",
-      createdAt: "2024-08-15T01:48:38.072Z",
-      updatedAt: "2024-08-15T07:37:13.578Z"
-    }
-  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -44,31 +23,6 @@ describe('ArticlesController', () => {
     expect(controller).toBeDefined();
   });
 
-  // Testing Create
-  it('should return new article', () => {
-    const newArticle = {
-      title: "Article dzf Testing",
-      description: "Just For Unit Test Article",
-      body: "Support unit Test for Article",
-      published: false
-    }
-    const result = controller.create(newArticle as CreateArticleDto);
-    expect(result).toBe(result);
-  });
-
-  // Testing Update
-  it('should return article', () => {
-    const updateArticle = {
-      id: 19,
-      title: "Article Testing",
-      description: "Just For Unit Test Auricle",
-      body: "Support unit Test for Article",
-      published: false
-    }
-    const result = controller.update(updateArticle.id.toString(), updateArticle as UpdateArticleDto);
-    expect(result).toEqual(result);
-  });
-
   // Testing Find All
   it('should return array articles', () => {
     const result = controller.findAll();
@@ -76,8 +30,8 @@ describe('ArticlesController', () => {
   });
 
   // Testing Find by ID
-  it('should return article', async () => {
-    const result = await controller.findOne(mockData.id);
+  it('should return article by ID', () => {
+    const result = controller.findOne(1);
     expect(result).toEqual(result); // Must Fix This Expected Result;
   });
 });
