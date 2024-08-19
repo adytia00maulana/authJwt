3a13ad13b8ffc68e874bd8b256b891dbb8dd162c
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-19 09:56:37 +0700

    add Unit Testing for Articles Service: add to Article Service Not Found Exception in find by Id

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,40 +1,43 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Injectable, Logger, NotFoundException } from "@nestjs/common";
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class ArticlesService {
-  constructor(private prisma: PrismaService, private readonly logger: Logger) {
+  constructor(private prismaService: PrismaService, private readonly logger: Logger) {
   }
   create(createArticleDto: CreateArticleDto) {
     this.logger.log(`${ArticlesService.name} - Create Successfully With Body ${JSON.stringify(createArticleDto)}`);
-    return this.prisma.article.create({ data: createArticleDto });
+    return this.prismaService.article.create({ data: createArticleDto });
   }
 
   findAll() {
     this.logger.log(`${ArticlesService.name} - Find All Published`);
-    return this.prisma.article.findMany({ where: { published: true } });
+    return this.prismaService.article.findMany({ where: { published: true } });
   }
 
   findDrafts() {
     this.logger.log(`${ArticlesService.name} - Find All Not Published`);
-    return this.prisma.article.findMany({ where: { published: false } });
+    return this.prismaService.article.findMany({ where: { published: false } });
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     this.logger.log(`${ArticlesService.name} - Find by Id ${id}`);
-    return this.prisma.article.findUnique({
+
+    const article = await this.prismaService.article.findUnique({
       where: { id },
       include: {
         author: true,
       },
     });
+    if(!article) throw new NotFoundException('User not found');
+    return article;
   }
 
   update(id: number, updateArticleDto: UpdateArticleDto) {
     this.logger.log(`${ArticlesService.name} - Update by Id ${id} With Body ${JSON.stringify(updateArticleDto)}`);
-    return this.prisma.article.update({
+    return this.prismaService.article.update({
       where: { id },
       data: updateArticleDto,
     });
@@ -42,6 +45,6 @@ export class ArticlesService {
 
   remove(id: number) {
     this.logger.log(`${ArticlesService.name} - Delete by Id ${id}`);
-    return this.prisma.article.delete({ where: { id } });
+    return this.prismaService.article.delete({ where: { id } });
   }
 }
