d6aa9cebc004d5bb73fc9651dc4c42b5700eb76e
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-20 17:03:57 +0700

    add module Currency: CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './modules/articles/articles.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { CurrencyModule } from './modules/integration/currency/currency.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, UsersModule, AuthModule],
+  imports: [PrismaModule, ArticlesModule, UsersModule, AuthModule, CurrencyModule],
   providers: [Logger],
 })
 export class AppModule {}
