81c2c1d7afe5ca1c846c53a2c8a38e2b31a20eaf
Adytia Maulana <adytia00maulana@gmail.com>
2024-08-22 16:15:20 +0700

    add Unit Testing User Controller

--- src/modules/users/users.controller.spec.ts
+++ src/modules/users/users.controller.spec.ts
@@ -7,6 +7,23 @@ import { Logger } from '@nestjs/common';
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const mockService = {
+    create: jest.fn(),
+    update: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn()
+  }
+
+  const mockUser = {
+    id: 1,
+    name: 'admin',
+    email: 'admin@example.com',
+    password: 'password',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -14,6 +31,10 @@ describe('UsersController', () => {
         UsersService,
         PrismaService,
         Logger,
+        {
+          provide: UsersService,
+          useValue: mockService
+        }
       ],
     }).compile();
 
@@ -24,15 +45,44 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
+  // Testing Create
+  it("should create user", async () => {
+    mockService.create.mockResolvedValue(mockUser);
+
+    const result = await controller.create(mockUser);
+    expect(result).toEqual(mockUser);
+  });
+
+  // Testing Update
+  it("should update user", async () => {
+    mockService.update.mockResolvedValue(mockUser);
+
+    const result = await controller.update(mockUser.id.toString(), mockUser);
+    expect(result).toEqual(mockUser);
+  });
+
   // Testing Find All
-  it('should return array user', () => {
-    const result = controller.findAll();
-    expect(result).toEqual(result); // Must Fix This Expected Result;
+  it('should return array user', async () => {
+    mockService.findAll.mockResolvedValue([mockUser]);
+
+    const result = await controller.findAll();
+    expect(result).toEqual([mockUser]);
+  });
+
+  it('should return empty array if there are no users', async () => {
+    mockService.findAll.mockResolvedValue([]);
+
+    const result = await controller.findAll();
+    expect(result).toEqual([]);
   });
 
   // Testing Find by ID
-  it('should return user', () => {
-    const result = controller.findOne(1);
-    expect(result).toEqual(result); // Must Fix This Expected Result;
+  it('should return user', async () => {
+    mockService.findOne.mockResolvedValueOnce(mockUser);
+
+    const result = await controller.findOne(mockUser.id);
+    expect(result).toEqual(mockUser);
+    expect(mockService.findOne).toHaveBeenCalledTimes(1);
+    expect(mockService.findOne).toHaveBeenCalledWith(mockUser.id);
   });
 });
